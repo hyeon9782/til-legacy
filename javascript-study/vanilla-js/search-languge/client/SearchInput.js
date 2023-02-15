df278cc5580d4db221aebd33d115715582273d36
hyeon9782 <hyeon9782@gmail.com>
2023-02-15 23:44:24 +0900

    Feat: vanilla-js 사원 정보 테이블 프로젝트 구성 및 기본 세팅

--- javascript-study/vanilla-js/search-languge/client/SearchInput.js
+++ javascript-study/vanilla-js/search-languge/client/SearchInput.js
@@ -4,27 +4,38 @@ export default function SearchInput({
     onChange
 }) {
 
-
+    
     this.$element = document.createElement('form');
     this.$element.className = "SearchInput";
     this.state = initialState;
+    
+    
 
     $target.appendChild(this.$element);
+    
+    
 
     this.render = () => {
+        
         this.$element.innerHTML = `<input class="SearchInput__input" type="text" placeholder="프로그래밍 언어를 입력하세요." value="${this.state}" />`
     }
+    
 
     this.render()
+    
+
+    this.$element.focus();
 
     this.$element.addEventListener('keyup', e => {
         const actionIgnoreKeys = ['Enter', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
 
         if (!actionIgnoreKeys.includes(e.key)) onChange(e.target.value);
     })
+    
 
     // submit 이벤트 무시하기
     this.$element.addEventListener('submit', e => {
         e.preventDefault()
     })
+    
 }
