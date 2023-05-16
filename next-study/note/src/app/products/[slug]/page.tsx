01a1ac3833d29c1cb51a89bc26105f584903aba7
hyeon9782 <hyeon9782@gmail.com>
2023-05-16 19:15:23 +0900

    feat: Next.js 렌더링 개념 학습

--- next-study/note/src/app/products/[slug]/page.tsx
+++ next-study/note/src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { getProduct, getProducts } from '@/service/products';
 import { generateKey } from 'crypto';
 import { notFound } from 'next/navigation';
 import React from 'react'
@@ -7,24 +8,29 @@ type Props = {
   }
 }
 
+export const revalidate = 3;
+
 export function generateMetadata({ params }: Props) {
   return {
     title: `제품의 이름: ${params.slug}`
   }
 }
 
-export default function PantsPage({ params }: Props) {
-  if (params.slug === 'nothing') {
-    notFound()
+export default async function ProductPage({ params: { slug } }: Props) {
+  // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
+  const product = await getProduct(slug)
+  if(!product) {
+    notFound();
   }
   return (
-    <div>{params.slug} 제품 페이지</div>
+    <div>{product.name} 제품 페이지</div>
   )
 }
 
-export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
+export async function generateStaticParams() {
+  // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줌(SSG)
+  const products = await getProducts()
   return products.map(product => ({
-    slug: product
+    slug: product.id
   }))
 }
--- next-study/note/src/app/products/page.tsx
+++ next-study/note/src/app/products/page.tsx
@@ -1,15 +1,20 @@
+import { getProducts } from '@/service/products'
 import Link from 'next/link'
 import React from 'react'
+import MeowArticle from '@/components/MeowArticle';
 
-const products = ['shirt', 'pants', 'skirt', 'shoes']
+// export const revalidate = 3;
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
+  const products = await getProducts();
   return (
     <>
       <h1>제품 소개 페이지</h1>
       <ul>
-        {products.map((product, index) => <li key={index}><Link href={`/products/${product}`}>{product}</Link></li>)}
+        {products.map((product, index) => <li key={index}><Link href={`/products/${product.id}`}>{product.name}</Link></li>)}
       </ul>
+      <MeowArticle />
     </>
   )
 }
