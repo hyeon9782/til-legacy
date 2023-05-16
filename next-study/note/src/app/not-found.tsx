3d1f8042889448309bc57855aa73d87dc8add5a2
hyeon9782 <hyeon9782@gmail.com>
2023-05-16 16:23:56 +0900

    faet: Next.js 페이지 구성 개념 학습

---
+++ next-study/note/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+export default function NotFoundPage() {
+  return (
+    <div>NotFoundPage 1314</div>
+  )
+}
---
+++ next-study/note/src/app/products/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { generateKey } from 'crypto';
+import { notFound } from 'next/navigation';
+import React from 'react'
+type Props = {
+  params: {
+    slug: string;
+  }
+}
+
+export function generateMetadata({ params }: Props) {
+  return {
+    title: `제품의 이름: ${params.slug}`
+  }
+}
+
+export default function PantsPage({ params }: Props) {
+  if (params.slug === 'nothing') {
+    notFound()
+  }
+  return (
+    <div>{params.slug} 제품 페이지</div>
+  )
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(product => ({
+    slug: product
+  }))
+}
